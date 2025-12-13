50b5de2b9bbc2f6a1b5cfa6a693e97ef9e4f10ef
liushijie <1751280328@qq.com>
2025-12-14 00:45:29 +0800

    feat: Add dispatch and steps curd

---
+++ src/api/collect/steps/index.ts
@@ -0,0 +1,42 @@
+import request from '/@/utils/request';
+
+
+export function getList(query:Object) {
+    return request({
+        url: '/api/v1/collect/steps/list',
+        method: 'get',
+        params: query
+    })
+}
+
+export function getItem(uuid:string) {
+    return request({
+        url: '/api/v1/collect/steps/get',
+        method: 'get',
+        params: {uuid}
+    })
+}
+
+export function addItem(data:any) {
+    return request({
+        url: '/api/v1/collect/steps/add',
+        method: 'post',
+        data: data
+    })
+}
+
+export function editItem(data:any) {
+    return request({
+        url: '/api/v1/collect/steps/edit',
+        method: 'put',
+        data: data
+    })
+}
+
+export function deleteItems(ids:string[]) {
+    return request({
+        url: '/api/v1/collect/steps/delete',
+        method: 'delete',
+        data: {ids}
+    })
+}
---
+++ src/views/collect/steps/component/edit.vue
@@ -0,0 +1,127 @@
+<template>
+	<div class="system-edit-dic-container">
+		<el-dialog :title="(ruleForm.uuid ? 'Edit' : 'Add')+' Steps'" v-model="isShowDialog" width="769px">
+			<el-form :model="ruleForm" ref="formRef" :rules="rules" size="default" label-width="90px">
+        <el-form-item label="Name" prop="name">
+          <el-input v-model="ruleForm.name" placeholder="Name" />
+        </el-form-item>
+        <el-form-item label="Type" prop="type">
+          <el-input v-model="ruleForm.type" placeholder="Type" />
+        </el-form-item>
+        <el-form-item label="Request" prop="request">
+          <el-input v-model="ruleForm.request" type="textarea" placeholder="Request" />
+        </el-form-item>
+        <el-form-item label="Response" prop="response">
+          <el-input v-model="ruleForm.response" type="textarea" placeholder="Response" />
+        </el-form-item>
+			</el-form>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button @click="onCancel" size="default">Cancel</el-button>
+					<el-button type="primary" @click="onSubmit" size="default">{{ruleForm.uuid ? 'Edit' : 'Add'}}</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs, defineComponent,ref, unref } from 'vue';
+import {ElMessage} from "element-plus";
+import {addItem, editItem, getItem} from "/@/api/collect/steps";
+interface RuleFormState {
+  uuid: string;
+  name: string;
+  type: string;
+  request: string;
+  response: string;
+}
+interface DicState {
+	isShowDialog: boolean;
+	ruleForm: RuleFormState;
+  rules: {}
+}
+
+export default defineComponent({
+	name: 'EditDispatchData',
+  props:{
+    sysYesNoOptions:{
+      type:Array,
+      default:()=>[]
+    }
+  },
+	setup(prop,{emit}) {
+    const formRef = ref<HTMLElement | null>(null);
+		const state = reactive<DicState>({
+			isShowDialog: false,
+			ruleForm: {
+        uuid: '',
+        name: '',
+        type: '',
+        request: '',
+        response: '',
+			},
+      rules: {
+        name: [
+          { required: true, message: "Name can not empty", trigger: "blur" },
+        ],
+      }
+		});
+		const openDialog = (row: RuleFormState|null) => {
+      resetForm();
+      if (row) {
+        getItem(row.uuid).then((res:any)=>{
+          const data:RuleFormState = res.data.data
+          state.ruleForm = data
+        })
+        state.ruleForm = row;
+      }
+			state.isShowDialog = true;
+		};
+    const resetForm = ()=>{
+      state.ruleForm = {
+        uuid: '',
+        name: '',
+        type: 'http',
+        request: '',
+        response: '',
+      }
+    };
+		const closeDialog = () => {
+			state.isShowDialog = false;
+		};
+		const onCancel = () => {
+			closeDialog();
+		};
+		const onSubmit = () => {
+      const formWrap = unref(formRef) as any;
+      if (!formWrap) return;
+      formWrap.validate((valid: boolean) => {
+        if (valid) {
+          if(!state.ruleForm.uuid){
+            addItem(state.ruleForm).then(()=>{
+              ElMessage.success('Add Success');
+              closeDialog();
+              emit('dataList')
+            })
+          }else{
+            editItem(state.ruleForm).then(()=>{
+              ElMessage.success('Edit Sucess');
+              closeDialog();
+              emit('dataList')
+            })
+          }
+        }
+      });
+		};
+		return {
+			openDialog,
+			closeDialog,
+			onCancel,
+			onSubmit,
+      formRef,
+			...toRefs(state),
+		};
+	},
+});
+</script>
